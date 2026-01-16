2b912e493563a17c725b4f5ecddecbeeb20234ce
Archana <archana24557@gmail.com>
2026-01-16 23:14:10 +0530

    Fix: Switch to BrowserRouter and add ErrorBoundary

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { HashRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
 import Navbar from "./components/Layout/Navbar";
 import Footer from "./components/Layout/Footer";
@@ -9,21 +9,52 @@ import BlogDetails from "./pages/blogs/BlogDetails";
 import CreateBlog from "./pages/blogs/CreateBlog";
 import EditBlog from "./pages/blogs/EditBlog";
 import About from "./pages/About";
+import { Component } from "react";
+
+class ErrorBoundary extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { hasError: false, error: null };
+  }
+
+  static getDerivedStateFromError(error) {
+    return { hasError: true, error };
+  }
+
+  componentDidCatch(error, errorInfo) {
+    console.error("ErrorBoundary caught an error", error, errorInfo);
+  }
+
+  render() {
+    if (this.state.hasError) {
+      return (
+        <div style={{ padding: "2rem", textAlign: "center", color: "white" }}>
+          <h1>Something went wrong.</h1>
+          <pre style={{ color: "red" }}>{this.state.error.toString()}</pre>
+        </div>
+      );
+    }
+
+    return this.props.children;
+  }
+}
 
 function App() {
   return (
-    <HashRouter>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/blogs" element={<Blogs />} />
-        <Route path="/blogs/:id" element={<BlogDetails />} />
-        <Route path="/create" element={<CreateBlog />} />
-        <Route path="/edit/:id" element={<EditBlog />} />
-        <Route path="/about" element={<About />} />
-      </Routes>
-      <Footer />
-    </HashRouter>
+    <BrowserRouter>
+      <ErrorBoundary>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/blogs" element={<Blogs />} />
+          <Route path="/blogs/:id" element={<BlogDetails />} />
+          <Route path="/create" element={<CreateBlog />} />
+          <Route path="/edit/:id" element={<EditBlog />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+        <Footer />
+      </ErrorBoundary>
+    </BrowserRouter>
   );
 }
 
